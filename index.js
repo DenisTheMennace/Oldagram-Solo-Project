cb73ac59501b8538fc736e4aa8183d397cd0738e
Denis Yurchenko <83593507+DenisTheMennace@users.noreply.github.com>
2024-03-25 20:42:29 -0400

    added all components, stretch goals incoming

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const posts = [
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
-        {
+    {
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
@@ -25,6 +25,33 @@ const posts = [
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
-    }
+    },
 ]
 
+const postsContainer = document.getElementById("posts-container")
+
+function renderPosts() {
+    for (let i = 0; i < posts.length; i++) {
+        postsContainer.innerHTML += `
+        <div class="post-header">
+            <img src="${posts[i].avatar}">
+            <div class="user-location-container">
+                <p class="user-name">${posts[i].name}</p>
+                <p class="location">${posts[i].location}</p>
+            </div>
+        </div>
+        <img class="post-image" src="${posts[i].post}">
+        <div class="post-footer">
+            <div class="icons">
+                <img src="images/icon-heart.png">
+                <img src="images/icon-comment.png">
+                <img src="images/icon-dm.png">
+            </div>
+            <p class="likes">${posts[i].likes} likes</p>
+            <p class="post-comment"><span>${posts[i].username}</span> ${posts[i].comment}</p>
+        </div>
+        `
+    }
+}
+
+renderPosts()
